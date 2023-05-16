c4df64c269bcf47cd5eb05061ee05f6b862b8e7f
cosmo <cosmoskitsune@hotmail.com>
2023-05-16 13:50:51 -0300

    mais migration para msqli

--- cadastro.php
+++ cadastro.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
 session_name("site");
+session_start();
 
 require_once("admin/includes/anti_injection.php");
 require_once("admin/includes/conecta_bd.php");
@@ -9,13 +9,13 @@ if(isset($_POST['acao']) && $_POST['acao'] == "enviar")
 {
     foreach ($_POST as $campo => $valor) { $$campo = $valor;}
 
-    //Veriável auxiliar para detectar spamns. Inicialmente é setada como false.
+    //Veriï¿½vel auxiliar para detectar spamns. Inicialmente ï¿½ setada como false.
     $spam = false;
 
     //echo "<pre>";
     //print_r($_POST);
     //exit;
-    //Verifica se os campos são vazios ou estão preenchidos com o conteúdo padrão.
+    //Verifica se os campos sï¿½o vazios ou estï¿½o preenchidos com o conteï¿½do padrï¿½o.
     if(empty($nome) || empty($cep) || empty($endereco1) || empty($nr_endereco) || empty($bairro) || empty($cidade) || empty($estado) || empty($fone) || empty($email))
     {
         echo("<script language='javascript'>\n alert('Favor preencher todos os campos')\n</script>");
@@ -32,22 +32,22 @@ if(isset($_POST['acao']) && $_POST['acao'] == "enviar")
 
            $total = 0;
            $cods = array();
-           $rs = mysql_query("SELECT cd_foto FROM carrinho_itens WHERE cd_carrinho='".$_SESSION['carrinho']."'");
-           while($var = mysql_fetch_array($rs))
+           $rs = mysqli_query($conn, "SELECT cd_foto FROM carrinho_itens WHERE cd_carrinho='".$_SESSION['carrinho']."'");
+           while($var = mysqli_fetch_array($rs, MYSQLI_ASSOC))
                 $cods[] = $var['cd_foto'];
 
            $cods = implode(',',$cods);
            
            if(!isset($_SESSION['carrinho']) || empty($_SESSION['carrinho']) || empty($cods))
            {
-                echo("<script language='javascript'>\n alert('Seu carrinho está vazio, favor selecionar as fotos desejadas antes de finalizar sua compra!')\n</script>");
+                echo("<script language='javascript'>\n alert('Seu carrinho estï¿½ vazio, favor selecionar as fotos desejadas antes de finalizar sua compra!')\n</script>");
                 echo("<script language='javascript'>location.href='solicitacao.php'</script>");
                 exit;
            }
            else
            {
-                $rs = mysql_query("INSERT INTO cliente_temp VALUES('','".$_SESSION['carrinho']."','$nome','$endereco1','$nr_endereco','$complemento','$cep',
-                '$bairro','$cidade','$estado','$email','$fone')") or die(mysql_error());
+                $rs = mysqli_query($conn, "INSERT INTO cliente_temp VALUES('','".$_SESSION['carrinho']."','$nome','$endereco1','$nr_endereco','$complemento','$cep',
+                '$bairro','$cidade','$estado','$email','$fone')") or die(mysqli_error());
             
                 echo("<script language='javascript'>location.href='solicitacao-confere.php'</script>");
                 exit;
@@ -57,7 +57,7 @@ if(isset($_POST['acao']) && $_POST['acao'] == "enviar")
 
     }
 }
-//função que verifica se o email foi escrito do formato correto voce@provedor.com
+//funï¿½ï¿½o que verifica se o email foi escrito do formato correto voce@provedor.com
 function verificar_email($email)
 {
 
