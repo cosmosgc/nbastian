61632758417b9ae4f886fd2286694ff18b494053
cosmo <cosmoskitsune@hotmail.com>
2023-08-22 15:22:56 -0300

    Perfis e Wordpress link

--- tangerina/load_people.php
+++ tangerina/load_people.php
@@ -2,19 +2,29 @@
 $peopleDirectory = 'people/';
 $people = [];
 
+$validImageExtensions = ['jpg', 'jpeg', 'png', 'gif']; // Add other valid image extensions
+
 foreach (scandir($peopleDirectory) as $file) {
-  if ($file !== '.' && $file !== '..' && pathinfo($file, PATHINFO_EXTENSION) === 'html') {
-    $personTitle = pathinfo($file, PATHINFO_FILENAME);
-    $personImagePath = $peopleDirectory . $personTitle . '.jpg'; // Change the image extension accordingly
-    $personImagePath = file_exists($personImagePath) ? $personImagePath : 'default-image.jpg'; // Provide a default image if not found
-    $people[] = [
-      'title' => $personTitle,
-      'path' => $peopleDirectory . $file,
-      'image' => $personImagePath
-    ];
+  if ($file !== '.' && $file !== '..') {
+    $pathInfo = pathinfo($file);
+    $fileExtension = strtolower($pathInfo['extension']);
+    if (in_array($fileExtension, $validImageExtensions) && $pathInfo['extension'] !== 'html') {
+      $personTitle = $pathInfo['filename'];
+      $personImagePath = $peopleDirectory . $personTitle . '.' . $fileExtension;
+      $defaultImagePath = 'default-image.jpg'; // Provide a default image if not found
+      if (!file_exists($personImagePath)) {
+        $personImagePath = $defaultImagePath;
+      }
+      $people[] = [
+        'title' => $personTitle,
+        'path' => $peopleDirectory . $personTitle . '.html', // Assuming HTML file has same name
+        'image' => $personImagePath
+      ];
+    }
   }
 }
 
 header('Content-Type: application/json');
 echo json_encode($people);
+
 ?>
